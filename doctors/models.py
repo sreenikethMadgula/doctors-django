b4a4f69c4b87ed4e370e75cf01e402892b96c1d6
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2022-08-13 07:09:46 +0530

    populated the database; need to make views and templates

---
+++ doctors/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Doctors(models.Model):
+    '''
+    schema: "name","degree","specialization","hospital","experience","awards"
+    '''
+    # id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255)
+    # hospital_id = models.IntegerField()
+    hospital = models.CharField(max_length=255)
+    experience = models.IntegerField()
+    awards = models.IntegerField(blank=True)
