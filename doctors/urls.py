2b5568ce7232d95b38064465087d58c31eea1db1
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2022-08-13 10:05:54 +0530

    most of the functionality implemented; categorical pages remaining

--- doctors/urls.py
+++ doctors/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('search_doctors',views.search_doctors, name="search-doctors"),
+    path('search_results',views.search_results,name="search-results"),
+    # path('doctor_profile',views.doctor_profile, name="doctor-profile"),
+    path('doctor_profile/<int:id>',views.doctor_profile, name="doctor-profile"),
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,8 +1,83 @@
+from re import search
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.template import loader,RequestContext
+from .models import Doctors
+from django.db.models import Q
+from django.views.decorators.csrf import *
 
 # Create your views here.
-def index(request):
+def home(request):
     template = loader.get_template('frontpage.html')
     return HttpResponse(template.render())   
+
+def search_doctors(request):
+    return render(
+        request,
+        'search_doctors.html',
+        {}
+    )
+
+# @csrf_protect
+@csrf_exempt
+def search_results(request):
+    # csrfContext = RequestContext(request)
+    if request.method=="POST":
+        search_doctor_query = request.POST['search_doctor_query']
+        doctors = Doctors.objects.filter(Q(name__icontains=search_doctor_query) | Q(specialization__icontains=search_doctor_query) | Q(hospital__icontains=search_doctor_query))
+        # doctors = Doctors.objects.values()
+        print(doctors)
+        if doctors:
+            return render(
+                request,
+                'search_results.html',
+                # csrfContext,
+                {
+                    "search_doctor_query":search_doctor_query,
+                    "doctors":doctors
+                    # "name":"cux",
+                    # "degree":"the",
+                    # "specialization":"mega",
+                    # "hospital":"only",
+                    # "experience":"45",
+                    # "awards":"99"
+                }
+        )
+        else:
+            return render(
+                request,
+                'no_result.html',
+                {
+                    "search_doctor_query":search_doctor_query,
+                }
+            )
+    else:
+        return render(
+            request,
+            'doctor_profile.html',
+            {
+                "name":"cux",
+                "degree":"the",
+                "specialization":"mega",
+                "hospital":"only",
+                "experience":"45",
+                "awards":"99"
+            }
+    )
+
+def doctor_profile(request,id):
+    doctor = Doctors.objects.get(id=id)
+    if(doctor):
+        return render(
+            request,
+            'doctor_profile.html',
+            {
+                "doctor":doctor
+            }
+        )       
+    else:
+        return render(
+            request,
+            HttpResponse("No profile found")
+        )
+        
