53d3e5af4be9bbe77ab2aa054db2c766bd2cd6cd
Sreeniketh Madgula <sreeniketh.madgula@gmail.com>
2022-08-15 23:10:39 +0530

    uff

--- doctors/views.py
+++ doctors/views.py
@@ -21,132 +21,96 @@ from .filters import DoctorFilter
 
 
 # Create your views here.
-def home(request):
-    template = loader.get_template('frontpage.html')
-    return HttpResponse(template.render())   
+# def home(request):
+#     template = loader.get_template('frontpage.html')
+#     return HttpResponse(template.render())   
 
-def search_doctors(request):
-    return render(
-        request,
-        'search_doctors.html',
-        {}
-    )
+# def search_doctors(request):
+#     return render(
+#         request,
+#         'search_doctors.html',
+#         {}
+#     )
 
-# @csrf_protect
-@csrf_exempt
-def search_results(request):
-    # csrfContext = RequestContext(request)
-    if request.method=="POST":
-        search_doctor_query = request.POST['search_doctor_query']
-        doctors = Doctors.objects.filter(Q(name__icontains=search_doctor_query) | Q(specialization__icontains=search_doctor_query) | Q(hospital__icontains=search_doctor_query))
-        # doctors = Doctors.objects.values()
-        print(doctors)
-        if doctors:
-            return render(
-                request,
-                'search_results.html',
-                # csrfContext,
-                {
-                    "search_doctor_query":search_doctor_query,
-                    "doctors":doctors
-                    # "name":"cux",
-                    # "degree":"the",
-                    # "specialization":"mega",
-                    # "hospital":"only",
-                    # "experience":"45",
-                    # "awards":"99"
-                }
-        )
-        else:
-            return render(
-                request,
-                'no_result.html',
-                {
-                    "search_doctor_query":search_doctor_query,
-                }
-            )
-    else:
-        return render(
-            request,
-            'doctor_profile.html',
-            {
-                "name":"cux",
-                "degree":"the",
-                "specialization":"mega",
-                "hospital":"only",
-                "experience":"45",
-                "awards":"99"
-            }
-    )
+# # @csrf_protect
+# @csrf_exempt
+# def search_results(request):
+#     # csrfContext = RequestContext(request)
+#     if request.method=="POST":
+#         search_doctor_query = request.POST['search_doctor_query']
+#         doctors = Doctors.objects.filter(Q(name__icontains=search_doctor_query) | Q(specialization__icontains=search_doctor_query) | Q(hospital__icontains=search_doctor_query))
+#         # doctors = Doctors.objects.values()
+#         print(doctors)
+#         if doctors:
+#             return render(
+#                 request,
+#                 'search_results.html',
+#                 # csrfContext,
+#                 {
+#                     "search_doctor_query":search_doctor_query,
+#                     "doctors":doctors
+#                     # "name":"cux",
+#                     # "degree":"the",
+#                     # "specialization":"mega",
+#                     # "hospital":"only",
+#                     # "experience":"45",
+#                     # "awards":"99"
+#                 }
+#         )
+#         else:
+#             return render(
+#                 request,
+#                 'no_result.html',
+#                 {
+#                     "search_doctor_query":search_doctor_query,
+#                 }
+#             )
+#     else:
+#         return render(
+#             request,
+#             'doctor_profile.html',
+#             {
+#                 "name":"cux",
+#                 "degree":"the",
+#                 "specialization":"mega",
+#                 "hospital":"only",
+#                 "experience":"45",
+#                 "awards":"99"
+#             }
+#     )
 
-def doctor_profile(request,id):
-    doctor = Doctors.objects.get(id=id)
-    if(doctor):
-        return render(
-            request,
-            'doctor_profile.html',
-            {
-                "doctor":doctor
-            }
-        )       
-    else:
-        return render(
-            request,
-            HttpResponse("No profile found")
-        )
-
-# @csrf_exemptx``
-def category_doctors(request,category):
-
-    # set up pagination
-    doctor_list = Doctors.objects.filter(specialization__icontains=category)
-    p = Paginator(doctor_list, 5)
-    page = request.GET.get('page')
-    doctors = p.get_page(page)
-    return render(
-        request,
-        'category_doctors.html',
-        {
-            "doctors":doctors
-        }
-    )
-
-
-# @api_view(['GET'])
-# def doctor_list(request):
-    # doctor_list = Doctors.objects.all()
-    # serializer = DoctorSerializer(doctor_list,many=True)
-    # return Response(serializer.data)
-
-# @api_view(['POST'])
-# def add_doctor(request):
-    # serializer = DoctorSerializer(data=request.data)
-    # if serializer.is_valid():
-    #     serializer.save()
-    #     return Response(serializer.data,status=status.HTTP_201_CREATED)
-
-# @api_view(['GET','PUT','DELETE'])
 # def doctor_profile(request,id):
-    # try:
-    #     doctor = Doctors.objects.get(pk=id)
-    # except Doctors.DoesNotExist:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
+#     doctor = Doctors.objects.get(id=id)
+#     if(doctor):
+#         return render(
+#             request,
+#             'doctor_profile.html',
+#             {
+#                 "doctor":doctor
+#             }
+#         )       
+#     else:
+#         return render(
+#             request,
+#             HttpResponse("No profile found")
+#         )
 
-#     if(request.method=='GET'):
-        # serializer = DoctorSerializer(doctor)
-        # return Response(serializer.data)
-    
-#     elif(request.method=='PUT'):
-        # serializer = DoctorSerializer(doctor, data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data)
-        # else:
-        #     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-#     elif(request.method=='DELETE'):
-        # doctor.delete()
-        # return Response(status=status.HTTP_204_NO_CONTENT)
+# # @csrf_exemptx``
+# def category_doctors(request,category):
+
+#     # set up pagination
+#     doctor_list = Doctors.objects.filter(specialization__icontains=category)
+#     p = Paginator(doctor_list, 5)
+#     page = request.GET.get('page')
+#     doctors = p.get_page(page)
+#     return render(
+#         request,
+#         'category_doctors.html',
+#         {
+#             "doctors":doctors
+#         }
+#     )
+
 
 
 class DoctorList(APIView):
--- healthXOXO/urls.py
+++ healthXOXO/urls.py
@@ -19,4 +19,5 @@ from django.urls import include,path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('doctors/', include('doctors.urls')),
+    path('', include('frontend.urls')),
 ]
